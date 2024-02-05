5884d19bae18733b4a9a6f94b512486cf2071446
Darth Degen <darthdegen@gmail.com>
2024-02-05 16:21:10 -0700

    feat: blem final review (pls) 2

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -29,10 +29,10 @@ const Footer: FC<Props> = (props: Props) => {
       key="footer"
     >
       <div className="flex max-[1000px]:flex-wrap gap-10 lg:gap-20 xl:gap-36 pl-16 max-[1000px]:pl-0">
-        <div className="flex flex-col gap-2 ">
-          <div className="font-mon-bold mb-2">ABOUT </div>
+        <div className="flex flex-col gap-4">
+          <div className="font-mon-bold  tracking-[2px]">ABOUT </div>
           <div
-            className="tracking-[2px] font-light cursor-pointer"
+            className="tracking-[2px] font-light cursor-pointer "
             onClick={() => navigate(2)}
           >
             MEET THE TEAM
@@ -46,7 +46,7 @@ const Footer: FC<Props> = (props: Props) => {
         </div>
 
         <div className="flex flex-col ">
-          <div className="font-mon-bold mb-4">PORTAL</div>
+          <div className="font-mon-bold mb-4 tracking-[2px]">PORTAL</div>
           <div className="tracking-[2px] font-light">
             <Link target="_blank" href="https://monarch.arkpes.com/login">
               INVESTOR PORTAL
@@ -55,7 +55,7 @@ const Footer: FC<Props> = (props: Props) => {
         </div>
 
         <div className="flex flex-col ">
-          <div className="font-mon-bold mb-4">CONTACT</div>
+          <div className="font-mon-bold mb-4 tracking-[2px]">CONTACT</div>
           <div className="tracking-[2px] font-light">
             <Link href="mailto:info@xyz.dev">INQUIRY FORM</Link>
           </div>
