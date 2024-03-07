d747c9b632d0b8c9b1a679e04b08b1c1a66edac3
Darth Degen <darthdegen@gmail.com>
2024-03-06 17:08:20 -0700

    feat: max w on news

--- src/components/templates/NewsView.tsx
+++ src/components/templates/NewsView.tsx
@@ -9,7 +9,7 @@ const NewsView: FC = () => {
       <h1 className="text-3xl tracking-[6px] py-10 md:py-20">NEWS</h1>
       <div className="w-[90%] flex gap-3 max-[1100px]:flex-col justify-center items-center  text-[#CDB7F6] text-xl lg:text-[28px] pb-[100px]">
         <NewsItem layout={0} article={NEWS_ARTICLES[0]} />
-        <div className="flex flex-col gap-3 items-center relative bottom-1">
+        <div className="flex flex-col gap-3 items-center relative">
           <NewsItem layout={1} article={NEWS_ARTICLES[1]} />
           <div className="flex gap-3 max-md:flex-col w-full justify-center">
             <NewsItem layout={2} article={NEWS_ARTICLES[2]} />
