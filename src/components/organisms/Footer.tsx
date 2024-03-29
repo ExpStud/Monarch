4a4152b3f81d77ff0cffdefb7fa73e68050b5bf1
Darth Degen <darthdegen@gmail.com>
2024-03-29 12:22:35 -0700

    feat: final udpates

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -25,10 +25,10 @@ const Footer: FC<Props> = (props: Props) => {
 
   return (
     <footer
-      className="px-10 xl:px-24 w-full bg-black z-0 text-[#CDB7F6] py-20 text-sm"
+      className="px-10 xl:px-10 w-full bg-black z-0 text-[#CDB7F6] py-20 lg:py-12 text-sm"
       key="footer"
     >
-      <div className="flex max-[1000px]:flex-wrap gap-10 lg:gap-20 xl:gap-36 pl-16 max-[1000px]:pl-0">
+      <div className="flex max-[1000px]:flex-wrap gap-10 lg:gap-20 xl:gap-36 pl-16 max-[1000px]:pl-0 sm:items-center sm:justify-evenly">
         <div className="flex flex-col gap-4">
           <div className="font-mon-bold  tracking-[2px]">ABOUT </div>
           <div
@@ -45,23 +45,26 @@ const Footer: FC<Props> = (props: Props) => {
           </div>
         </div>
 
-        <div className="flex flex-col ">
-          <div className="font-mon-bold mb-4 tracking-[2px]">PORTAL</div>
+        <div className="flex flex-col gap-4">
+          <div className="font-mon-bold  tracking-[2px]">OTHER</div>
           <div className="tracking-[2px] font-light">
             <Link target="_blank" href="https://monarch.arkpes.com/login">
               INVESTOR PORTAL
             </Link>
           </div>
-        </div>
-
-        <div className="flex flex-col ">
-          <div className="font-mon-bold mb-4 tracking-[2px]">CONTACT</div>
           <div className="tracking-[2px] font-light">
             <Link href="mailto:info@xyz.dev">INQUIRY FORM</Link>
           </div>
         </div>
 
-        <div className="flex grow max-[1000px]:justify-start justify-end items-end tracking-[2px] font-mon-bold whitespace-nowrap">
+        {/* <div className="flex flex-col ">
+          <div className="font-mon-bold mb-4 tracking-[2px]">CONTACT</div>
+          <div className="tracking-[2px] font-light">
+            <Link href="mailto:info@xyz.dev">INQUIRY FORM</Link>
+          </div>
+        </div> */}
+
+        <div className="flex grow justify-center md:justify-end items-end tracking-[2px] font-mon-bold whitespace-nowrap mt-10 lg:mt-0">
           <Link target="_blank" href="https://twitter.com/exp_studio_">
             POWERED BY EXP
           </Link>
