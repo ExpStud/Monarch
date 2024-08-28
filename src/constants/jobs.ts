b7929008f8f622ffde59ebfb06bdac97b5060b45
Darth Degen <darthdegen@gmail.com>
2024-08-28 16:45:55 -0700

    removed job posting

--- src/constants/jobs.ts
+++ src/constants/jobs.ts
@@ -8,11 +8,11 @@ export const jobPositions:Jobs[] = [
     description: "Monarch Collective is a $150M investment fund focused on investing in women's sports teams, leagues, and rights. Our mission is to build women's sports institutions as modern communities, with the belief that we can learn from sports best practices while building electrifying experiences that feel different and representative of this time in history.",
     url: "https://airtable.com/app8w0Py8Rg4HYWIi/pagr3jpHU3MQ3SJHG/form"
 }, 
-{
-  date: "7/29/2024",
-  title: "Ops & Marketing Lead",
-  tags: ["Remote", "Full-time"],
-  description: "Monarch Collective is a $150M investment fund focused on investing in women's sports teams, leagues, and rights. Our mission is to build women's sports institutions as modern communities, with the belief that we can learn from sports best practices while building electrifying experiences that feel different and representative of this time in history.",
-  url: "https://airtable.com/appu4l1etyamIPsX0/pagNBEBnfKG84JfWo/form"
-},
+// {
+//   date: "7/29/2024",
+//   title: "Ops & Marketing Lead",
+//   tags: ["Remote", "Full-time"],
+//   description: "Monarch Collective is a $150M investment fund focused on investing in women's sports teams, leagues, and rights. Our mission is to build women's sports institutions as modern communities, with the belief that we can learn from sports best practices while building electrifying experiences that feel different and representative of this time in history.",
+//   url: "https://airtable.com/appu4l1etyamIPsX0/pagNBEBnfKG84JfWo/form"
+// },
 ]
